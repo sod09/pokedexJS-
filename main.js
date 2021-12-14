b2c9b5d0aab41be3be5d3a841d89b6a4e57d556c
Sasha O'Donovan <sasha.odonovan@gmail.com>
2021-12-14 09:28:32 +0000

    displays info

--- main.js
+++ main.js
@@ -16,14 +16,36 @@ const getPokemonInfo = (pokemon) => {
     .then((response) => response.json())
     .then((pokeInfo) => {
       document.body.innerHTML += getPokeCard(pokeInfo);
+      console.log(pokeInfo);
     });
 };
 
+// const getPokemonTypes = (pokemon) => {
+//   let url = pokemon.url;
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((pokeTypes) => {
+//       return `<li>${pokeTypes.types}</li>`;
+//     });
+// };
+
 const getPokeCard = (pokeInfo) => {
+  const pokeNameCaps = pokeInfo.name.toUpperCase();
+
+  const pokeTypes = pokeInfo.types
+    .map((type) => {
+      const pokeType = type.type.name;
+      return pokeType;
+    })
+    .join(", ");
+
   return `
+  <div class="pokecontainer">
   <div class="pokecard">
-  <p>Id: ${pokeInfo.id}</p>
-  <h1 class="pokename">Name: ${pokeInfo.name}</h1>
-  <img src="${pokeInfo.sprites.front_default}"</img>
+  
+  <h1 class="pokename">${pokeNameCaps}</h1>
+  <img class="pokeimg" src="${pokeInfo.sprites.front_default}"</img>
+  <div class=poketypes>Type: ${pokeTypes}</div>
+  </div>
   </div>`;
 };
